16720f98e2cb1a892f1053b5226a270e18509506
kongfang_ubt <1515623212@qq.com>
2025-10-01 18:40:17 +0800

    update

--- Algorithm Design and Analysis Homework/week2/A.cpp
+++ Algorithm Design and Analysis Homework/week2/A.cpp
@@ -9,7 +9,10 @@ typedef unsigned long long ull;
 const int mod = 1e9+7;
 //const int mod = 998244353;
 const int MAX_N = 1;
-
+/*题解：
+想把n个圆盘从a通过b移动到c，得先把n-1个圆盘从a通过c移动到b，
+再把第n个圆盘从a直接移动到c，最后把n-1个圆盘从b通过a移动到c
+*/
 int T=1;
 int n;
 string s="ABC";
@@ -23,7 +26,6 @@ void Hannoi(int a,int b,int c,int n){
 		Hannoi(b,a,c,n-1);
 	}
 }
-
 
 int main(){
 	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
