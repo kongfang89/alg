9b3900601baa609b63143d782a928f453a29faf1
kongfang_ubt <1515623212@qq.com>
2025-10-02 15:29:39 +0800

    update

--- Algorithm Design and Analysis Homework/week3/C.cpp
+++ Algorithm Design and Analysis Homework/week3/C.cpp
@@ -9,16 +9,22 @@ typedef unsigned long long ull;
 const int mod = 1e9+7;
 //const int mod = 998244353;
 const int MAX_N = 200010;
-
+//题解：看注释
 int T=1;
 //点的结构体 
 struct point{
 	double x,y;
     int id;
 }p[MAX_N];
-vector<pair<double,pair<int,int>>> ans;
 //点的个数，存放两个平面中间部分的点的编号 
 int n,a[MAX_N];
+//算过的全存进ans，最后排序输出最小的（因为我不想每次更新mind的时候比序号大小= =）
+struct node{
+	double d;
+	int id1,id2;
+};
+vector<node> ans;
+
 //点排序函数（一开始垂直划分两个平面 
 bool cmp1(point a,point b){
 	if(a.x==b.x)
@@ -40,8 +46,8 @@ double solve(int l,int r){
 		return INF;
 	//如果区间只剩两个点，直接返回两点距离 
 	if(l+1==r){
-        ans.push_back({dist(p[l],p[r]),{min(p[l].id,p[r].id),max(p[l].id,p[r].id)}});
-        return dist(p[l],p[r]);
+        ans.push_back({dist(p[l],p[r]),min(p[l].id,p[r].id),max(p[l].id,p[r].id)});
+        return ans.back().d;
     }
 	//否则根据中间的点垂直划分出两个平面 
 	int mid=(l+r)/2;
@@ -63,35 +69,37 @@ double solve(int l,int r){
 		//获取第一个点后面所有的点（如果两点y值之差超过mind，筛去） 
 		for(int j=i+1;j<=cnt&&p[a[j]].y-p[a[i]].y<=mind;j++)
 			//计算并更新最小值 
-			if(dist(p[a[j]],p[a[i]])<mind){
+			if(dist(p[a[j]],p[a[i]])<=mind){
                 mind=dist(p[a[i]],p[a[j]]);
-                ans.push_back({mind,{min(p[a[i]].id,p[a[j]].id),max(p[a[i]].id,p[a[j]].id)}});
+				int id1=min(p[a[i]].id,p[a[j]].id);
+				int id2=max(p[a[i]].id,p[a[j]].id);
+                ans.push_back({mind,min(p[a[i]].id,p[a[j]].id),max(p[a[i]].id,p[a[j]].id)});
             }
 	//返回 
 	return mind;
 }
-
-int cmp(pair<double,pair<int,int>> a,pair<double,pair<int,int>> b){
-    if(a.first==b.first){
-        if(a.second.first==b.second.first)
-            return a.second.second<b.second.second;
-        return a.second.first<b.second.first;
+//答案排序函数
+int cmp(node a,node b){
+    if(a.d==b.d){
+        if(a.id1==b.id1)
+            return a.id2<b.id2;
+        return a.id1<b.id1;
     }
-    return a.first<b.first;
+    return a.d<b.d;
 }
 
 int main(){
 	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
 	// cin>>T;
     while(cin>>n){
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<n;i++){
             cin>>p[i].x>>p[i].y;
-            p[i].id=i-1;
+            p[i].id=i;
         }
-        sort(p+1,p+n+1,cmp1);
-        double ansd=solve(1,n);
+        sort(p,p+n,cmp1);
+        double ansd=solve(0,n-1);
         sort(ans.begin(),ans.end(),cmp);
-        cout<<ans[0].second.first<<' '<<ans[0].second.second<<'\n';
+        cout<<ans[0].id1<<' '<<ans[0].id2<<'\n';
         ans.clear();
     }
 	return 0;
