41169aa470576425d2b99e67656ab8c93479eb91
kongfang_ubt <1515623212@qq.com>
2025-10-02 11:46:36 +0800

    update

--- Algorithm Design and Analysis Homework/week2/F.cpp
+++ Algorithm Design and Analysis Homework/week2/F.cpp
@@ -9,7 +9,7 @@ typedef unsigned long long ull;
 const int mod = 1e9+7;
 //const int mod = 998244353;
 const int MAX_N = 1e5+10;
-
+//题解：模拟题，递归实现仿蝴蝶变换即可
 int T=1;
 int n,a[MAX_N],b[MAX_N];
 
@@ -29,11 +29,13 @@ void ButterflyTrans(int l,int r){
 
 int main(){
 	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
-	cin>>n;
-	for(int i=1;i<=n;i++)
-		cin>>a[i];
-	ButterflyTrans(1,n);
-	for(int i=1;i<=n;i++)
-		cout<<a[i]<<" \n"[i==n];
+	// cin>>T;
+	while(cin>>n){
+		for(int i=1;i<=n;i++)
+			cin>>a[i];
+		ButterflyTrans(1,n);
+		for(int i=1;i<=n;i++)
+			cout<<a[i]<<" \n"[i==n];
+	}
 	return 0;
 }
