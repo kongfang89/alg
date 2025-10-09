e7c175b4b125cd04770c45a1aff58bf07c8fb7cb
kongfang_ubt <1515623212@qq.com>
2025-10-09 21:41:09 +0800

    update

---
+++ ATC2/25.10.9 abc AtCoder Beginner Contest 421/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LINF LLONG_MAX 
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod = 1e9+7;
+//const int mod = 998244353;
+const int MAX_N = 20;
+
+int T=1;
+int a[MAX_N];
+
+int rev(int x){
+	int res=0;
+	while(x){
+		res*=10;
+		res+=x%10;
+		x/=10;
+	}
+	return res;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+	// cin>>T;
+	while(T--){
+		cin>>a[1]>>a[2];
+		for(int i=3;i<=10;i++)
+			a[i]=rev(a[i-1]+a[i-2]);
+		cout<<a[10]<<'\n';
+	}
+	return 0;
+}
