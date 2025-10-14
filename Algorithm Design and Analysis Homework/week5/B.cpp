cc60e0d2c7ec1c0b1fcda20030ba3cc961948e4f
kongfang_ubt <1515623212@qq.com>
2025-10-14 19:22:59 +0800

    update

---
+++ Algorithm Design and Analysis Homework/week5/B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LINF LLONG_MAX 
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod = 1e9+7;
+//const int mod = 998244353;
+const int MAX_N = 1e4+10;
+
+int T=1;
+int n,b;
+int w[MAX_N],v[MAX_N];
+int dp[MAX_N];
+
+int main(){
+	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+	// cin>>T;
+	while(cin>>n>>b){
+		for(int i=1;i<=n;i++)
+			cin>>w[i]>>v[i];
+		memset(dp,0,sizeof(dp));
+		for(int i=1;i<=n;i++)
+			for(int j=b;j>=w[i];j--)
+				dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
+		cout<<dp[b]<<'\n';
+	}
+	return 0;
+}
---
+++ Algorithm Design and Analysis Homework/week5/C.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LINF LLONG_MAX 
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod = 1e9+7;
+//const int mod = 998244353;
+const int MAX_N = 1e3+10;
+
+int T=1;
+int n,b;
+int a[MAX_N],dp[MAX_N];
+
+int main(){
+	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+	// cin>>T;
+	while(cin>>n>>b){
+		a[1]=b;
+		for(int i=2;i<=n;i++)
+			a[i]=1LL*(a[i-1]+1)*(a[i-1]+1)%mod;
+		int ans=0;
+		for(int i=1;i<=n;i++){
+			dp[i]=1;
+			for(int j=1;j<i;j++)
+				if(a[j]<a[i]&&dp[j]+1>dp[i])
+					dp[i]=dp[j]+1;
+			ans=max(ans,dp[i]);
+		}
+		cout<<ans<<'\n';
+	}
+	return 0;
+}
