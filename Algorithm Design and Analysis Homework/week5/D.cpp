c5069a7c7ba0e444bd3690b2b76c44d7504e4e66
kongfang_ubt <1515623212@qq.com>
2025-10-21 13:11:52 +0800

    update

--- Algorithm Design and Analysis Homework/week5/D.cpp
+++ Algorithm Design and Analysis Homework/week5/D.cpp
@@ -8,36 +8,33 @@ typedef long long ll;
 typedef unsigned long long ull;
 const int mod = 1e9+7;
 //const int mod = 998244353;
-const int MAX_N = 1e3+10;
+const int MAX_N = 1e6+10;
 /*题解：
-dp[i][j]表示s的前i个字符与t的前j个字符的最长公共子序列长度。
-则如果s[i]==t[j]，dp[i][j]=dp[i-1][j-1]+1；
-否则dp[i][j]=max(dp[i-1][j],dp[i][j-1])，表示从s的前i-1个字符与t的前j个字符的最长公共子序列长度，
-和s的前i个字符与t的前j-1个字符的最长公共子序列长度中取较大值。
+tail[i]表示长度为i的上升子序列的最小末尾元素
+使用贪心+二分查找优化
 */
 int T=1;
-string s,t;
-int dp[MAX_N][MAX_N];
+int n,b;
+int a[MAX_N];
+int tail[MAX_N];
 
 int main(){
 	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
 	// cin>>T;
-	while(cin>>s>>t){
-        int n=s.size(),m=t.size();
-        s=" "+s;
-        t=" "+t;
-		dp[0][0]=0;
-        dp[1][0]=0;
-        dp[0][1]=0;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i]==t[j])
-                    dp[i][j]=dp[i-1][j-1]+1;
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        cout<<dp[n][m]<<'\n';
+	while(cin>>n>>b){
+		a[1]=b;
+		for(int i=2;i<=n;i++)
+			a[i]=1LL*(a[i-1]+1)*(a[i-1]+1)%mod;
+		int len=0;
+		for(int i=1;i<=n;i++){
+			if(len==0||a[i]>tail[len])
+				tail[++len]=a[i];
+			else{
+				int pos=lower_bound(tail+1,tail+1+len,a[i])-tail;
+				tail[pos]=a[i];
+			}
+		}
+		cout<<len<<'\n';
 	}
 	return 0;
 }
