7f7736a745f46c30c8d2f77fd4cb734c1e195330
kongfang_ubt <1515623212@qq.com>
2025-10-27 10:50:25 +0800

    update

---
+++ ATC2/25.10.27 abc AtCoder Beginner Contest 418/A.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LINF LLONG_MAX 
+#define YES cout<<"Yes"<<endl
+#define NO cout<<"No"<<endl
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod = 1e9+7;
+//const int mod = 998244353;
+const int MAX_N = 1;
+
+int T=1;
+int n;
+string s;
+
+int main(){
+	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+	// cin>>T;
+	while(T--){
+		cin>>n>>s;
+        if(n<3){
+            NO;
+            continue;
+        }
+        s=s.substr(n-3,3);
+        if(s=="tea")
+            YES;
+        else
+            NO;
+	}
+	return 0;
+}
