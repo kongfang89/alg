2a26fb79fb1efc4d47e1e58bbc8bbbe2d2555372
kongfang_ubt <1515623212@qq.com>
2025-10-29 14:34:17 +0800

    update

--- Algorithm Design and Analysis Homework/week7/H.cpp
+++ Algorithm Design and Analysis Homework/week7/H.cpp
@@ -9,7 +9,13 @@ typedef unsigned long long ull;
 const int mod = 1e9+7;
 //const int mod = 998244353;
 const int MAX_N = 100;
-
+/*题解：
+统计总长，最差情况就是全部拼成一根；然后从大到小排序，因为先用大的肯定更好拼；记录每个长度相同的木棒的最后位置，如果这根拼不出来，等长的也拼不出来；
+枚举总长的因数，不是因数也拼不出来；dfs搜索，从第一根木棒开始，如果这根拼完了，且是最后一根就结束，不是就找当前可用的最大木棒继续拼；
+如果这根没拼完，二分找当前可用的最大木棒，从这根开始一根根尝试继续拼；
+如果当前剩下的长度和用的木棒长度相等，继续拼却失败了，说明不用继续了，因为用小的肯定不如用大的好拼；
+如果当前剩下的长度等于要拼的长度，说明新的要拼的还没拼，继续拼却失败了，说明这根木棒完全用不了，那也不用继续了
+*/
 int T=1;
 int n,m,len,a[MAX_N],sum,nxt[MAX_N],vis[MAX_N],flag;
 
@@ -49,7 +55,7 @@ void dfs(int now,int last,int rest){
         vis[i]=0;
         if(flag)
             return;
-        if(rest==a[i]||rest==m)
+        if(rest==a[i]||rest==len)
             return;
         i=nxt[i];
         if(i==n)
