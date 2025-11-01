a9b5a9acd585a636c83290cf7abb670bdd1d7ef0
kongfang_ubt <1515623212@qq.com>
2025-11-01 14:35:07 +0800

    update

--- ATC2/25.11.1 abc AtCoder Beginner Contest 417/C.cpp
+++ ATC2/25.11.1 abc AtCoder Beginner Contest 417/C.cpp
@@ -8,15 +8,29 @@ typedef long long ll;
 typedef unsigned long long ull;
 const int mod = 1e9+7;
 //const int mod = 998244353;
-const int MAX_N = 1;
+const int MAX_N = 2e5+10;
 
 int T=1;
+int n,a[MAX_N];
+map<int,int> mp1,mp2;
 
 int main(){
 	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
-	cin>>T;
+	// cin>>T;
 	while(T--){
-		
+		cin>>n;
+        for(int i=1;i<=n;i++){
+            cin>>a[i];
+            mp1[a[i]+i]++;
+            if(i-a[i]>0)
+                mp2[i-a[i]]++;
+        }
+        ll ans=0;
+        for(auto [x,y]:mp1){
+            if(mp2[x]>0)
+                ans+=1ll*y*mp2[x];
+        }
+        cout<<ans<<'\n';
 	}
 	return 0;
 }
