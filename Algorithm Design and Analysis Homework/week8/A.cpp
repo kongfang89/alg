58bf6ac65c588ff76d260fd63287a7bbc00ed506
kongfang_ubt <1515623212@qq.com>
2025-11-04 19:17:07 +0800

    update

---
+++ Algorithm Design and Analysis Homework/week8/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LINF LLONG_MAX 
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod = 1e9+7;
+//const int mod = 998244353;
+const int MAX_N = 110;
+
+int T=1;
+int n,m,M[MAX_N][MAX_N],vis[MAX_N],ans;
+
+void dfs(int now,int cnt){
+    if(now==n+1){
+        ans=max(ans,cnt);
+        return;
+    }
+    if(cnt+(n-(now-1))<=ans)
+        return;
+    int flag=1;
+    for(int i=1;i<=n;i++)
+        if(vis[i]&&!M[now][i])
+            flag=0;
+    if(flag){
+        vis[now]=1;
+        dfs(now+1,cnt+1);
+    }
+    vis[now]=0;
+    dfs(now+1,cnt);
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+	// cin>>T;
+	while(T--){
+        cin>>n>>m;
+        for(int i=1;i<=m;i++){
+            int u,v;
+            cin>>u>>v;
+            M[u][v]=1;
+            M[v][u]=1;
+        }
+        ans=0;
+        dfs(1,0);
+        cout<<ans<<'\n';
+	}
+	return 0;
+}
---
+++ Algorithm Design and Analysis Homework/week8/B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define INF INT_MAX
+#define LINF LLONG_MAX 
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod = 1e9+7;
+//const int mod = 998244353;
+const int MAX_N = 20;
+
+int T=1;
+int n,m,M[MAX_N][MAX_N],vis[MAX_N],minedge[MAX_N],p[MAX_N],ans;
+
+void dfs(int now,int val){
+	if(now==n+1){
+        val+=M[p[n]][1];
+		ans=min(ans,val);
+		return;
+	}
+	int res=0;
+	for(int i=1;i<=n;i++){
+		if(vis[i])
+			continue;
+		res+=minedge[i];
+	}
+	if(val+res>=ans)
+		return;
+	for(int i=1;i<=n;i++){
+        if(vis[i])
+            continue;
+        vis[i]=1;
+        p[now]=i;
+        dfs(now+1,val+M[p[now-1]][i]);
+        vis[i]=0;
+    }
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+	// cin>>T;
+	while(T--){
+		cin>>n;
+		m=n*(n-1)/2;
+		for(int i=1;i<=m;i++){
+			int u,v,w;
+			cin>>u>>v>>w;
+			M[u][v]=w;
+			M[v][u]=w;
+			minedge[u]=min(minedge[u],w);
+			minedge[v]=min(minedge[v],w);
+		}
+		ans=0x3f3f3f3f;
+        vis[1]=1;
+        p[1]=1;
+		dfs(2,0);
+		cout<<ans<<'\n';
+	}
+	return 0;
+}
