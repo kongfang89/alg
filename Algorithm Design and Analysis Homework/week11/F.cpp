5025df55aceb684478fc42699466c7e79420b1c1
kongfang_ubt <1515623212@qq.com>
2025-11-25 13:17:58 +0800

    update

--- Algorithm Design and Analysis Homework/week11/F.cpp
+++ Algorithm Design and Analysis Homework/week11/F.cpp
@@ -10,9 +10,9 @@ const int mod = 1e9+7;
 //const int mod = 998244353;
 const int MAX_N = 1010;
 const double eps=1e-9;
-
+//题解：枚举所有直线的交点，判断交点是否合法，合法则计算x+y的最小值
 int T=1;
-
+int n;
 struct Line{
 	double a,b,c;
 };
@@ -35,38 +35,38 @@ bool check(double x,double y,vector<Line>& lines) {
 
 int main(){
 	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
-	int n;
-	cin>>n;
-	vector<Line> lines;
-	for(int i=0;i<n;i++){
-		double a,b,c;
-		cin>>a>>b>>c;
-		lines.push_back({a,b,c});
-	}
-	lines.push_back({-1,0,0});
-	lines.push_back({0,-1,0});
-	int m=lines.size();
-	vector<pair<double,double>> v;
-	for(int i=0;i<m;i++){
-		for(int j=i+1;j<m;j++){
-			auto [x,y]=intersect(lines[i],lines[j]);
-			if(x<INF-1&&y<INF-1)
-				if(x>-eps&&y>-eps&&check(x,y,lines))
-					v.push_back({x,y});
+	// cin>>T;
+	while(T--){
+		cin>>n;
+		vector<Line> lines;
+		for(int i=1;i<=n;i++){
+			double a,b,c;
+			cin>>a>>b>>c;
+			lines.push_back({a,b,c});
 		}
-	}
-	if(v.empty()){
-		cout<<"Not Found\n";
-		return 0;
-	}
-	double ans=INF;
-	for(auto [x,y]:v)
-		ans=min(ans,x+y);
-	if(ans>=INF-1){
-		cout<<"Not Found\n";
-	}
-	else{
-		cout<<(ll)ans<<endl;
+		lines.push_back({-1,0,0});
+		lines.push_back({0,-1,0});
+		int m=lines.size();
+		vector<pair<double,double>> v;
+		for(int i=0;i<m;i++){
+			for(int j=i+1;j<m;j++){
+				auto [x,y]=intersect(lines[i],lines[j]);
+				if(x<INF&&y<INF&&x>-eps&&y>-eps)
+					if(check(x,y,lines))
+						v.push_back({x,y});
+			}
+		}
+		if(v.empty()){
+			cout<<"Not Found\n";
+			return 0;
+		}
+		double ans=INF;
+		for(auto [x,y]:v)
+			ans=min(ans,x+y);
+		if(ans>=INF-1)
+			cout<<"Not Found\n";
+		else
+			cout<<(ll)ans<<endl;
 	}
 	return 0;
 }
