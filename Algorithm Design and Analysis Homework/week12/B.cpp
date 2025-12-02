d1f9e3573395ef27126b39b02e636357fe4ada0d
kongfang_ubt <1515623212@qq.com>
2025-12-02 18:07:47 +0800

    update

--- Algorithm Design and Analysis Homework/week12/B.cpp
+++ Algorithm Design and Analysis Homework/week12/B.cpp
@@ -1,86 +1,83 @@
 #include <bits/stdc++.h>
+#define INF INT_MAX
+#define LINF LLONG_MAX 
+#define YES cout<<"YES"<<endl
+#define NO cout<<"NO"<<endl
 using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int mod = 1e9+7;
+//const int mod = 998244353;
+const int MAX_N = 1e5+10;
 
-const long long INF = 4e18;
-const int MAXN = 100000 + 5;
+int T=1;
+int n,k;
+int a[MAX_N];
+ll dp[21][MAX_N];
+ll cnt[MAX_N];
+ll cost=0;
+int curl=1,curr=0;
 
-long long dp[21][MAXN];
-int a[MAXN];
-long long freq[MAXN];
-long long curCost = 0;
-int curL = 1, curR = 0;    // 当前有效范围为空
-
-// 将值 x 加入当前区间
-inline void add(int x) {
-    curCost += freq[x];
-    freq[x]++;
+void add(int x) {
+    cost+=cnt[x];
+    cnt[x]++;
 }
 
-// 将值 x 从当前区间移除
-inline void remove_(int x) {
-    freq[x]--;
-    curCost -= freq[x];
+void remov(int x) {
+    cnt[x]--;
+    cost-=cnt[x];
 }
 
-// 调整区间到 [l, r]
-inline void moveTo(int l, int r) {
-    while (curL > l) add(a[--curL]);
-    while (curR < r) add(a[++curR]);
-    while (curL < l) remove_(a[curL++]);
-    while (curR > r) remove_(a[curR--]);
+void change(int l,int r){
+    while(curl>l)
+        add(a[--curl]);
+    while(curr<r)
+        add(a[++curr]);
+    while(curl<l)
+        remov(a[curl++]);
+    while(curr>r)
+        remov(a[curr--]);
 }
 
-// 分治优化求 dp[t][L..R]
-void solve(int t, int L, int R, int optL, int optR) {
-    if (L > R) return;
-    int mid = (L + R) >> 1;
-
-    long long bestVal = INF;
-    int bestPos = -1;
-
-    int start = min(mid - 1, optR);
-
-    // 枚举断点 j，区间为 (j+1 .. mid)
-    for (int j = optL; j <= start; j++) {
-        moveTo(j + 1, mid);
-        long long val = dp[t - 1][j] + curCost;
-        if (val < bestVal) {
-            bestVal = val;
-            bestPos = j;
+void solve(int t,int L,int R,int l,int r){
+    if(L>R)
+        return;
+    int mid=L+R>>1;
+    ll mi=1e18;
+    int id=-1;
+    int start=min(mid-1,r);
+    for(int j=l;j<=start;j++){
+        change(j+1,mid);
+        ll val=dp[t-1][j]+cost;
+        if(val<mi){
+            mi=val;
+            id=j;
         }
     }
-
-    dp[t][mid] = bestVal;
-
-    solve(t, L, mid - 1, optL, bestPos);
-    solve(t, mid + 1, R, bestPos, optR);
+    dp[t][mid]=mi;
+    solve(t,L,mid-1,l,id);
+    solve(t,mid+1,R,id,r);
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n, k;
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++) cin >> a[i];
-
-    // 初始化 DP
-    for (int i = 0; i <= k; i++)
-        for (int j = 0; j <= n; j++)
-            dp[i][j] = INF;
-
-    dp[0][0] = 0;
-
-    for (int t = 1; t <= k; t++) {
-        // 清空频率数组和代价
-        memset(freq, 0, sizeof(long long) * (n + 2));
-        curL = 1;
-        curR = 0;
-        curCost = 0;
-
-        solve(t, 1, n, 0, n - 1);
+int main(){
+    ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
+    // cin>>T;
+    while(T--){
+        cin>>n>>k;
+        for(int i=1;i<=n;i++)
+            cin>>a[i];
+        for(int i=0;i<=k;i++)
+            for(int j=0;j<=n;j++)
+                dp[i][j]=1e18;
+        dp[0][0]=0;
+        for(int i=1;i<=k;i++){
+            memset(cnt,0,sizeof(cnt));
+            curl=1;
+            curr=0;
+            cost=0;
+            solve(i,1,n,0,n-1);
+        }
+        cout<<dp[k][n]<<'\n';
     }
-
-    cout << dp[k][n] << "\n";
     return 0;
 }
