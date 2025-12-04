05203cff4e445e26f1155058aeb781a72ac54dcc
kongfang_ubt <1515623212@qq.com>
2025-12-04 13:55:32 +0800

    update

--- Algorithm Design and Analysis Homework/week12/D.cpp
+++ Algorithm Design and Analysis Homework/week12/D.cpp
@@ -9,10 +9,11 @@ typedef unsigned long long ull;
 const int mod = 1e9+7;
 //const int mod = 998244353;
 const int MAX_N = 100;
-
+//题解：dp[i][0]表示从第i块馅饼开始轮到Bob先手时Bob能获得的最大分数，dp[i][1]表示从第i块馅饼开始轮到Alice先手时Bob能获得的最大分数
 int T=1;
 int n;
 int a[MAX_N];
+int dp[MAX_N][2];
 
 int main(){
 	ios_base::sync_with_stdio(false);cout.tie(0);cin.tie(0);
@@ -24,22 +25,15 @@ int main(){
             cin>>a[i];
             sum+=a[i];
         }
-        vector<vector<int>> dp(n+10,vector<int>(2,0));
-        for(int i=n;i>=1;i--){
-            if(i==n){
-                dp[i][0]=a[i];
-                dp[i][1]=0;
-            } 
-            else{
-                int res1=a[i]+dp[i+1][1];
-                int res2=dp[i+1][0];
-                dp[i][0]=max(res1, res2);
-                res1=dp[i+1][0];
-                res2=a[i]+dp[i+1][1];
-                dp[i][1]=min(res1, res2);
-            }
+        dp[n][0]=a[n];
+        dp[n][1]=0;
+        for(int i=n-1;i>=1;i--){
+            int res1=a[i]+dp[i+1][1];
+            int res2=dp[i+1][0];
+            dp[i][0]=max(res1,res2);
+            dp[i][1]=min(res1,res2);
         }
-        cout<<sum-dp[1][0]<<" "<<dp[1][0]<<'\n';
+        cout<<sum-dp[1][0]<<' '<<dp[1][0]<<'\n';
 	}
 	return 0;
 }
